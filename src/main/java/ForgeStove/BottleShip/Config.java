94514b7202995f0602b6c237907df541a0aa4b38
ForgeStove <2071948808@qq.com>
2025-02-11 23:08:56 +0800

    style(Config): 使用静态导入简化代码

--- src/main/java/ForgeStove/BottleShip/Config.java
+++ src/main/java/ForgeStove/BottleShip/Config.java
@@ -3,15 +3,17 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jetbrains.annotations.NotNull;
+
+import static net.minecraftforge.common.ForgeConfigSpec.Builder;
 public class Config {
 	public static ConfigValue<Integer> bottleWithoutShipChargeTime;
 	public static ConfigValue<Integer> bottleWithoutShipCooldown;
 	public static ConfigValue<Integer> bottleWithShipChargeTime;
 	public static ConfigValue<Integer> bottleWithShipCooldown;
 	public static ConfigValue<Integer> bottleWithShipChargeStrength;
-	public static final Pair<Config, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Config::new);
+	public static final Pair<Config, ForgeConfigSpec> specPair = new Builder().configure(Config::new);
 	public static final ForgeConfigSpec CONFIG_SPEC = specPair.getValue();
-	public Config(ForgeConfigSpec.@NotNull Builder builder) {
+	public Config(@NotNull Builder builder) {
 		bottleWithoutShipChargeTime = builder.defineInRange("bottleWithoutShipChargeTime/ms", 1000, 0, 72000);
 		bottleWithoutShipCooldown = builder.defineInRange("bottleWithoutShipCooldown/tick", 60, 0, Integer.MAX_VALUE);
 		bottleWithShipChargeTime = builder.defineInRange("bottleWithShipChargeTime/ms", 1000, 0, 72000);
