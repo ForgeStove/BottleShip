bdc59890438e7a59e1f5b9122f8dedad075397e0
ForgeStove <2071948808@qq.com>
2025-02-11 23:09:07 +0800

    refactor: 使用静态导入简化方法调用

--- src/main/java/ForgeStove/BottleShip/Teleport.java
+++ src/main/java/ForgeStove/BottleShip/Teleport.java
@@ -1,11 +1,12 @@
 package ForgeStove.BottleShip;
 import net.minecraft.server.level.ServerLevel;
 import net.spaceeye.vmod.utils.Vector3d;
-import net.spaceeye.vmod.utils.vs.TeleportShipWithConnectedKt;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3dc;
 import org.valkyrienskies.core.api.ships.ServerShip;
-import org.valkyrienskies.mod.common.VSGameUtilsKt;
+
+import static net.spaceeye.vmod.utils.vs.TeleportShipWithConnectedKt.teleportShipWithConnected;
+import static org.valkyrienskies.mod.common.VSGameUtilsKt.getDimensionId;
 public class Teleport {
 	public static void teleportShip(
 			@NotNull ServerLevel level,
@@ -15,13 +16,13 @@ public class Teleport {
 			double z
 	) {
 		Vector3dc scaling = ship.getTransform().getShipToWorldScaling();
-		TeleportShipWithConnectedKt.teleportShipWithConnected(
+		teleportShipWithConnected(
 				level,
 				ship,
 				new Vector3d(x, y, z),
 				ship.getTransform().getShipToWorldRotation(),
 				(scaling.x() + scaling.y() + scaling.z()) / 3,
-				VSGameUtilsKt.getDimensionId(level)
+				getDimensionId(level)
 		);
 	}
 }
