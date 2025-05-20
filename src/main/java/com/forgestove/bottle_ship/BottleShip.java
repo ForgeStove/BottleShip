c60f9d1830de2255846439105c5dc9fb7256bf51
ForgeStove <ForgeStove@outlook.com>
2025-05-20 11:06:14 +0800

    fix: update lambda parameters in displayItems method for clarity

--- src/main/java/com/forgestove/bottle_ship/BottleShip.java
+++ src/main/java/com/forgestove/bottle_ship/BottleShip.java
@@ -26,7 +26,7 @@ public class BottleShip {
 			"tab." + ID,
 			() -> CreativeModeTab.builder().title(Component.translatable("tab." + ID))
 								 .icon(() -> BOTTLE_WITH_SHIP.get().getDefaultInstance())
-								 .displayItems((_, output) -> output.accept(BOTTLE_WITHOUT_SHIP.get())).build()
+								 .displayItems((parameters, output) -> output.accept(BOTTLE_WITHOUT_SHIP.get())).build()
 		);
 	}
 	public BottleShip() {
