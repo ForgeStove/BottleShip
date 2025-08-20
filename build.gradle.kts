28690edca26794d27fe546ee9ded185e9d676c72
ForgeStove <ForgeStove@outlook.com>
2025-08-20 16:21:22 +0800

    更新 build.gradle.kts 更新 Gradle Wrapper 至 8.14.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,10 @@ plugins {
 	idea
 	id("net.neoforged.moddev.legacyforge") version "+"
 	id("me.modmuss50.mod-publish-plugin") version "+"
-//	id("dev.vfyjxf.modaccessor") version "+"
 }
-base.archivesName.set(e("mod_id"))
-group = e("mod_group_id")
-version = "${e("minecraft_version")}-${e("mod_version")}+${e("upper_loader")}"
+base.archivesName.set(p("mod_id"))
+group = p("mod_group_id")
+version = "${p("minecraft_version")}-${p("mod_version")}+${p("upper_loader")}"
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 idea.module {
 	isDownloadSources = true
@@ -16,7 +15,7 @@ idea.module {
 }
 tasks.jar {
 	from("LICENSE")
-//	manifest { attributes(mapOf("MixinConfigs" to "${e("mod_id")}.mixins.json")) }
+//	manifest { attributes(mapOf("MixinConfigs" to "${p("mod_id")}.mixins.json")) }
 }
 tasks.processResources {
 	val replace = properties.mapValues { it.value.toString() }
@@ -28,19 +27,11 @@ tasks.processResources {
 	into("build/resources/main")
 	duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
-//modAccessor {
-//	createTransformConfiguration(configurations.compileOnly.get())
-//	accessTransformerFiles = legacyForge.accessTransformers.files
-//}
-//mixin {
-//	add(sourceSets.main.get(), "${e("mod_id")}.refmap.json")
-//	config("${e("mod_id")}.mixins.json")
-//}
 legacyForge {
-	version = "${e("minecraft_version")}-${e("loader_version")}"
+	version = "${p("minecraft_version")}-${p("loader_version")}"
 	parchment {
-		mappingsVersion.set(e("parchment_version"))
-		minecraftVersion.set(e("minecraft_version"))
+		mappingsVersion.set(p("parchment_version"))
+		minecraftVersion.set(p("minecraft_version"))
 	}
 	runs {
 		create("client") { client() }
@@ -49,7 +40,7 @@ legacyForge {
 			systemProperty("terminal.jline", "true")
 		}
 	}
-	mods { create(e("mod_id")) { sourceSet(sourceSets["main"]) } }
+	mods { create(p("mod_id")) { sourceSet(sourceSets["main"]) } }
 }
 repositories {
 	mavenLocal()
@@ -60,31 +51,31 @@ repositories {
 	maven("https://maven.blamejared.com") // JEI
 }
 dependencies {
-	modImplementation("maven.modrinth:valkyrien-skies:1.20.1-forge-2.3.0-beta.7")
-	modImplementation("maven.modrinth:vmod:lK0oLDaV")
+	modImplementation("maven.modrinth:valkyrien-skies:1.20.1-forge-2.3.0-beta.9")
+	modImplementation("maven.modrinth:vmod:16KEVTxj")
 	modImplementation("maven.modrinth:kotlin-for-forge:4.11.0")
 	modImplementation("maven.modrinth:architectury-api:9.2.14+forge")
 	compileOnly("org.joml:joml-primitives:1.10.0")
 	compileOnly(fileTree("libs"))
-	modImplementation("me.shedaniel.cloth:cloth-config-${e("loader")}:${e("cloth_config_version")}")
-	modRuntimeOnly("mezz.jei:jei-${e("minecraft_version")}-${e("loader")}:${e("jei_version")}")
-	compileOnly("org.jetbrains:annotations:${e("annotations_version")}")
-//	compileOnly("io.github.llamalad7:mixinextras-common:${e("mixin_extras_version")}")
-//	implementation("io.github.llamalad7:mixinextras-${e("loader")}:${e("mixin_extras_version")}")
-//	annotationProcessor("org.spongepowered:mixin:${e("mixin_version")}:processor")
+	modImplementation("me.shedaniel.cloth:cloth-config-${p("loader")}:${p("cloth_config_version")}")
+	modRuntimeOnly("mezz.jei:jei-${p("minecraft_version")}-${p("loader")}:${p("jei_version")}")
+	compileOnly("org.jetbrains:annotations:${p("annotations_version")}")
+//	compileOnly("io.github.llamalad7:mixinextras-common:${p("mixin_extras_version")}")
+//	implementation("io.github.llamalad7:mixinextras-${p("loader")}:${p("mixin_extras_version")}")
+//	annotationProcessor("org.spongepowered:mixin:${p("mixin_version")}:processor")
 }
 publishMods {
 	file.set(tasks.named("reobfJar").get().outputs.files.singleFile)
 	changelog.set(file("CHANGELOG.md").readText())
 	type.set(STABLE)
 	version.set(project.version.toString())
-	displayName.set("[${e("upper_loader")}] ${e("mod_name")} ${e("mod_version")}+${e("minecraft_version")}")
-	modLoaders.addAll(e("upper_loader"))
+	displayName.set("[${p("upper_loader")}] ${p("mod_name")} ${p("mod_version")}+${p("minecraft_version")}")
+	modLoaders.addAll(p("upper_loader"))
 	modrinth {
 		accessToken.set(providers.environmentVariable("MODRINTH_TOKEN"))
 		projectId.set("1o5XPZYT")
-		minecraftVersions.add(e("minecraft_version"))
+		minecraftVersions.add(p("minecraft_version"))
 		requires("valkyrien-skies", "vmod")
 	}
 }
-fun e(key: String) = extra[key].toString()
+fun p(key: String) = property(key).toString()
